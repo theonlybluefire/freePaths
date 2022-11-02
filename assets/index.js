bec8c8d47f8b6d2c2122647012180c277c421704
white47583 <white.privat@gmail.com>
2022-11-02 13:16:45 +0100

    added localStorage moves, but new issue #1

---
+++ assets/index.js
@@ -0,0 +1,40 @@
+/* Rules : 
+Blocks
+0 Lines for blocks (things who belong together)
+1 Line for things in one big group, but who don't belong together
+2 Lines for seperat blocks, who aren't in a group
+
+Headings
+Group Blocks heading will be written 1 Line before the other blocks
+Normal heading in or out of a group will be written 0 Lines before the block
+*/
+
+
+//deklaration arrays/variable
+var moves = ['turnVault', 'reverseVault']
+
+
+//addToArray
+function addtoArray(arrayname, input_id) {
+    let input = document.getElementById(input_id).value;
+    arrayname.push(input_id);
+}
+
+
+//get/set from Local Storage
+function addArraytoLocalStorage(arrayname, localStorageName) {
+    window.localStorage.clear()
+    window.localStorage.setItem(localStorageName, arrayname);
+}
+function getArrayfromLocalStorage(arrayname, localStorageName) {
+    arrayname = null
+    arrayname = window.localStorage.getItem(localStorageName);
+    window.localStorage.clear
+}
+
+//output
+function output(js_output, id_to_add) {
+    let add_to_HTML = document.getElementById(id_to_add).innerHTML += `
+    ${js_output}
+    `
+}
