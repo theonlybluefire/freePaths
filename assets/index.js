614119053664ea23673174ea1c39fde1b4ecca8c
white47583 <white.privat@gmail.com>
2022-11-11 22:52:28 +0100

    Added auswahlbildschrim with idea #4/#5+load array

--- assets/index.js
+++ assets/index.js
@@ -39,3 +39,50 @@ function output(js_output, id_to_add) {
     `
 }
 */
+//definition
+
+var Basic_movements = [
+    'Landing',
+    'Parkour Roll',
+    'Cat Leap',
+    'Dash Vault',
+    'Monkey Vault',
+    'Kong Vault',
+    'Speed Vault',
+    'Kash Vault',
+    'Two Handed Vault',
+    'Turn Vault',
+    'Precision Jump',
+    'Lazy Vault',
+    'Reverse Vault',
+    'Underbar',
+    'Tic Tac',
+    'strides']
+
+
+
+
+
+
+//buttons
+
+function start_planing() {
+    window.location = "../sites/choose_skills.html"
+}
+
+
+function load_array(arrayname, divname_outer, lenght_array) { //Bei lenght_array mit 0 zählen heißt einfach alle Werte zählen mit Zero
+    var i2 = 0
+    let div_container_outer = document.getElementById(divname_outer);
+    if (i2==0) {
+    //einfügen der benötigten Elemente
+        for ( let i=0; i<lenght_array; i++) {
+            div_container_outer.innerHTML += `<a href="#" class="list-group-item list-group-item-action" id="addClass"></a>`
+            let addClass_value = document.getElementById('addClass').classList.add('list_item_',i);
+            let div_container_inner = document.getElementsByTagName('a')[i];
+            div_container_inner.innerHTML += arrayname[i];
+            i2 = 1
+        }
+    }
+}
+
