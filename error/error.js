7550a4f9dcb8b093680c4e53d79435861b387e83
white47583 <white.privat@gmail.com>
2022-12-12 20:06:38 +0100

    Added Login require with Netlify Identity

---
+++ error/error.js
@@ -0,0 +1,10 @@
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('open', () => console.log('Widget opened'));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+netlifyIdentity.open();
+function openModal() {
+    netlifyIdentity.open();
+}
