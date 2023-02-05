603b129a513937ffa78490ad74ae86cc094397a5
white <white.privat@gmail.com>
2023-02-05 12:23:13 +0100

    home page desgin + name + netlify integration+ #14

--- assets/index.js
+++ assets/index.js
@@ -75,6 +75,64 @@ function checked(test) {
 
 
 
+//onload
+function onload() {
+    if(window.sessionStorage.getItem('loaded')=='true') {
+        
+        let removeClassOne = document.getElementById('removeOne').classList.remove('onloadOne');
+        let removeClassTwo = document.getElementById('removeTwo').classList.remove('onloadTwo');
+        let removeClassThree = document.getElementById('body').classList.remove('onloadThree');
+        let removeClassFour = document.getElementById('removeFour').classList.remove('onloadFour');
+    
+        
+    }
+    else {
+        window.sessionStorage.setItem('loaded','true');
+    }
+
+    netlifyIdentity.on('init', user => console.log('init', user));
+    netlifyIdentity.on('login', user => console.log('login', user));
+    netlifyIdentity.on('logout', () => console.log('Logged out'));
+    netlifyIdentity.on('error', err => console.error('Error', err));
+    netlifyIdentity.on('open', () => console.log('Widget opened'));
+    netlifyIdentity.on('close', () => console.log('Widget closed'));
+    setTimeout(function(){
+        const user = netlifyIdentity.currentUser();
+    },200)
+
+}
+//animations
+function buttonAni(id,side) {
+    let button = document.getElementById(id)
+    button.classList.add('buttonAni');
+    setTimeout(function() {
+        window.location = side;
+    },350)
+}
+//button start
+function startPlaning() {
+    const user = netlifyIdentity.currentUser();
+    if(user==!null) {
+        window.location = '../sites/choose_skills.html'
+
+    }
+    else{
+        netlifyIdentity.open('login')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //definition
 let clicked ='false'
@@ -108,9 +166,6 @@ var Basic_movements_onclick_clicked_color
 
 //buttons
 
-function startPlaning() {
-    window.location = "../sites/choose_skills.html"
-}
 
 //Array
 function addItem() {
@@ -137,11 +192,4 @@ function loadArraySelf() {
     }
 }
 
-//animations
-function buttonAni(id,side) {
-    let button = document.getElementById(id)
-    button.classList.add('buttonAni');
-    setTimeout(function() {
-        window.location = side;
-    },350)
-}
+
