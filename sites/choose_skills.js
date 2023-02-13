3e4010cedc715b2a29231012c7ccc0ab38c66f6e
white <white.privat@gmail.com>
2023-02-13 20:17:22 +0100

    added value input abfrage + new issue #12

--- sites/choose_skills.js
+++ sites/choose_skills.js
@@ -1,18 +1,17 @@
 //add Item
-var selfAddedValues = ['test'
+var selfAddedValues = [
 ]
 var div = []
 
 
 
 function addItem() {
-    let ItemInput = document.getElementsByTagName('input')[0].value;
-    console.log(ItemInput);
-    console.log('added value');
-    selfAddedValues.push(ItemInput);
-    console.log(selfAddedValues);
-    localStorage.setItem("selfAddedValues", JSON.stringify(selfAddedValues));
-
+    let input = document.getElementsByTagName('input')[0].value
+    console.log('Value added : ',input);
+    selfAddedValues.push(input);
+    console.log('Value pushed in Array, Array : ', input)
+    localStorage.setItem("selfAddedValues", JSON.stringify(input));
+    console.log('data saved')
     //const data = JSON.parse(localStorage.getItem("selfAddedValues"));
 }
 
@@ -20,7 +19,7 @@ function addItem() {
 function start(divid){
     console.log(JSON.parse(localStorage.getItem("selfAddedValues")));
     var selfAddedValues = JSON.parse(localStorage.getItem("selfAddedValues"));
-    console.log(selfAddedValues)
+    console.log('Array after saving and searching : ',selfAddedValues)
     for(i=0;i<selfAddedValues.length;i++) {
         div[i] = document.getElementById(divid).innerHTML += `<button type="button" class="list-group-item list-group-item-action">${selfAddedValues[i]}</button>`
     }
