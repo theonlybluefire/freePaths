b3f44473e6cdf2ac9c214493f964e205f02a480f
white <white.privat@gmail.com>
2023-02-22 20:31:49 +0100

    solved complete issue #16 + #15, + design

--- assets/index.js
+++ assets/index.js
@@ -1,96 +1,15 @@
-/* Rules : 
-Blocks
-0 Lines for blocks (things who belong together)
-1 Line for things in one big group, but who don't belong together
-2 Lines for seperat blocks, who aren't in a group
-
-Headings
-Group Blocks heading will be written 1 Line before the other blocks
-Normal heading in or out of a group will be written 0 Lines before the block
-*/
-
-/*
-//deklaration arrays/variable
-var moves = ['turnVault', 'reverseVault']
-
-
-//addToArray
-function addtoArray(arrayname, input_id) {
-    let input = document.getElementById(input_id).value;
-    arrayname.push(input_id);
-}
-
-
-//get/set from Local Storage
-function addArraytoLocalStorage(arrayname, localStorageName) {
-    window.localStorage.clear()
-    window.localStorage.setItem(localStorageName, arrayname);
-}
-function getArrayfromLocalStorage(arrayname, localStorageName) {
-    arrayname = null
-    arrayname = window.localStorage.getItem(localStorageName);
-    window.localStorage.clear
-}
-
-//output
-function output(js_output, id_to_add) {
-    let add_to_HTML = document.getElementById(id_to_add).innerHTML += `
-    ${js_output}
-    `
-}
-
-
-
-
-
-
-
-
-
-function load_array(arrayname, divname_outer, lenght_array, function_for_onclick) { 
-    if(clicked=='false') {
-        let div_container_outer = document.getElementById(divname_outer);
-        for ( var i=0; i<lenght_array; i++) {
-            div_container_outer.innerHTML += `<a href="#" class="list-group-item list-group-item-action" id="addClass"></a>`
-            let addClass_value = document.getElementsByTagName('a')[i].classList.add(i);
-            let div_container_inner = document.getElementsByTagName('a')[i];
-            div_container_inner.innerHTML += arrayname[i];  
-        }
-        Basic_movements_onclick[i] = document.getElementsByTagName('a')[0].onclick = function() {
-            if (Basic_movements_onclick_clicked[0] == false) {
-                Basic_movements_onclick_clicked_color[0] = document.getElementsByTagName['a'][0].style.backgroundColor = "green";
-                //ich möchte bei jedem Element eine neue Funktion einrichten, allerdings wird somit ein limit gegeben 
-
-            }
-        
-        }
-    clicked = 'true';
-    }
-}
-function checked(test) {
-    console.log(test);
-}
-*/
-//init netlify identity
-
-
-
 //onload
 function onload() {
     if(window.sessionStorage.getItem('loaded')=='true') {
-        
         let removeClassOne = document.getElementById('removeOne').classList.remove('onloadOne');
         let removeClassTwo = document.getElementById('removeTwo').classList.remove('onloadTwo');
         let removeClassThree = document.getElementById('body').classList.remove('onloadThree');
         let removeClassFour = document.getElementById('removeFour').classList.remove('onloadFour');
         let removeClassFive = document.getElementById('removeFive').classList.remove('onloadFive');
-    
-        
     }
     else {
         window.sessionStorage.setItem('loaded','true');
     }
-
     netlifyIdentity.on('init', user => console.log('init', user));
     netlifyIdentity.on('login', user => console.log('login', user));
     netlifyIdentity.on('logout', () => console.log('Logged out'));
@@ -100,8 +19,6 @@ function onload() {
     setTimeout(function(){
         const user = netlifyIdentity.currentUser();
     },200)
-
-
     //animated start button
     var buttonToAnimate = document.getElementById("startButton");
     buttonToAnimate.addEventListener('mousemove', e => {
@@ -116,8 +33,12 @@ function onload() {
 function buttonAni(id,side) {
     let button = document.getElementById(id)
     button.classList.add('buttonAni');
+    
     setTimeout(function() {
-        window.location = side;
+        let body = document.getElementById('body').classList.add('disappear')
+        setTimeout(function() {
+            window.location='../sites/choose_skills.html'
+        },450)
     },350)
 }
 //button start
@@ -129,34 +50,6 @@ function startPlaning() {
     }
     else{
         netlifyIdentity.open('login')
-    }
-}
-
-
-
-
-//Array
-function addItem() {
-    let ItemInput = document.getElementsByTagName('input')[0].value;
-    console.log(ItemInput);
-    console.log('added value');
-    self_aded_values.push(ItemInput);
-    window.localStorage.setItem('self_aded_values',self_aded_values);
-
-}
-function loadArraySelf() {
-    self_aded_values = localStorage.getItem('self_aded_values');
-    console.log(self_aded_values);
-    lenght_array = 2
-    if(clicked=='false') {
-        let div_container_outer = document.getElementById('self_added_values_list');
-        for ( var i=0; i<lenght_array; i++) {
-            div_container_outer.innerHTML += `<a href="#" class="list-group-item list-group-item-action" id="addClass"></a>`
-            let addClass_value = document.getElementsByTagName('a')[i].classList.add(i);
-            let div_container_inner = document.getElementsByTagName('a')[i];
-            div_container_inner.innerHTML += self_aded_values[i];  
-        }
-    clicked = 'true';
     }
 }
 
--- sites/choose_skills.js
+++ sites/choose_skills.js
@@ -1,65 +1,30 @@
 
+//definition
+sessionStorage.setItem('onloadAnimationOne','true')
 var div = []
-if(JSON.parse(localStorage.getItem('selfAddedValuesKey'))==null) {
-    var addSomething = ['']
-    localStorage.setItem("selfAddedValuesKey", JSON.stringify(addSomething)); 
+//check if it is nesersary to add a NullValue
+if(JSON.parse(localStorage.getItem('MovesICanKey'))==null) {
+    var addNullValue = ['']
+    localStorage.setItem("MovesICanKey", JSON.stringify(addNullValue)); 
 }
-var selfAddedValues=JSON.parse(localStorage.getItem("selfAddedValuesKey"));
-
+//define array
+var MovesICan=JSON.parse(localStorage.getItem("MovesICanKey"));
+//onload start
 function start(divid) {
-    for(i=0;i<selfAddedValues.length;i++) {
-        div[i] = document.getElementById(divid).innerHTML += `<button type="button" class="list-group-item list-group-item-action">${selfAddedValues[i]}</button>`
+    for(i=0;i<MovesICan.length;i++) {
+        div[i] = document.getElementById(divid).innerHTML += `<button type="button" class="list-group-item list-group-item-action designList">${MovesICan[i]}</button>`
     }
 }
-
-
+//add Item to list
 function addItem() {
     let input = document.getElementById('inputId').value;
-    console.log('inputValue',input);
-    selfAddedValues.push(input);
-    console.log('Array after push', selfAddedValues);
-    input.value = "";
+    MovesICan.push(input);
+    let del = document.getElementById('inputId').value = null;
 }
-
+//reload and save
 function reload() {
-    localStorage.setItem("selfAddedValuesKey", JSON.stringify(selfAddedValues));
-    console.log('localStorage Entry',JSON.parse(localStorage.getItem("selfAddedValuesKey")));
+    localStorage.setItem("MovesICanKey", JSON.stringify(MovesICan));
     setTimeout(function() {
         location.reload();
     },100)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function addItem() {
-    let input = document.getElementsByTagName('input')[0].value
-    console.log('Value added : ',input);
-    selfAddedValues.push(input);
-    console.log('Value pushed in Array, Array : ', input)
-    localStorage.setItem("selfAddedValues", JSON.stringify(input));
-    console.log('data saved')
-    //const data = JSON.parse(localStorage.getItem("selfAddedValues"));
-}
-
-
-function start(divid){
-    console.log(JSON.parse(localStorage.getItem("selfAddedValues")));
-    var selfAddedValues = JSON.parse(localStorage.getItem("selfAddedValues"));
-    console.log('Array after saving and searching : ',selfAddedValues)
-    for(i=0;i<selfAddedValues.length;i++) {
-        div[i] = document.getElementById(divid).innerHTML += `<button type="button" class="list-group-item list-group-item-action">${selfAddedValues[i]}</button>`
-    }
-}*/
